c5587a43f8ae8c436e8ec812ecec2ead98399d88
Damiano Chintala <damianochintala@gmail.com>
2020-03-26 04:39:36 +0200

    Updates

---
+++ precache-manifest.7357e2bfd751ae5f2c5525645075d558.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51b0779a1c210e265debfcf9c74d619b",
+    "url": "/matsiz/index.html"
+  },
+  {
+    "revision": "b41d087aad11104a7efa",
+    "url": "/matsiz/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "bcdf586f75c208d8cb50",
+    "url": "/matsiz/static/css/main.980c81eb.chunk.css"
+  },
+  {
+    "revision": "b41d087aad11104a7efa",
+    "url": "/matsiz/static/js/2.0387a286.chunk.js"
+  },
+  {
+    "revision": "6e71b6ee5ea7775d33431df95d229f09",
+    "url": "/matsiz/static/js/2.0387a286.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcdf586f75c208d8cb50",
+    "url": "/matsiz/static/js/main.1c0db3a5.chunk.js"
+  },
+  {
+    "revision": "2bfae217e71ed0c61518",
+    "url": "/matsiz/static/js/runtime-main.75c994ed.js"
+  },
+  {
+    "revision": "1016eb73ef6069571202b28a2a727b73",
+    "url": "/matsiz/static/media/back1.1016eb73.jpg"
+  },
+  {
+    "revision": "5b654988b85931ccee5b9f2a17b60f3f",
+    "url": "/matsiz/static/media/back2.5b654988.jpg"
+  },
+  {
+    "revision": "217ccab31f4494f88ab1cac6fadd3084",
+    "url": "/matsiz/static/media/car1.217ccab3.jpg"
+  },
+  {
+    "revision": "5c5db67b58ae4e697d307c1ca98d04e9",
+    "url": "/matsiz/static/media/car2.5c5db67b.jpg"
+  },
+  {
+    "revision": "241f9fdf1fcf8bba8da90f5d77af08f5",
+    "url": "/matsiz/static/media/car3.241f9fdf.jpg"
+  },
+  {
+    "revision": "e8453ab2824def789b094e03f0099d01",
+    "url": "/matsiz/static/media/car4.e8453ab2.jpg"
+  },
+  {
+    "revision": "e742df014d54dffd80b765f0972e5d4b",
+    "url": "/matsiz/static/media/car5.e742df01.jpg"
+  },
+  {
+    "revision": "766aec818789e2f585b12b911acbb21f",
+    "url": "/matsiz/static/media/car6.766aec81.jpg"
+  },
+  {
+    "revision": "334f4ddceeda3a4706fa75c6975bc326",
+    "url": "/matsiz/static/media/car7.334f4ddc.jpg"
+  },
+  {
+    "revision": "1ef21992a3efd81b66f3ae57b9124e34",
+    "url": "/matsiz/static/media/logo.1ef21992.jpg"
+  },
+  {
+    "revision": "03117b29c3d5d581494728b98b245dcd",
+    "url": "/matsiz/static/media/us1.03117b29.jpg"
+  },
+  {
+    "revision": "bd68b88b00e068e8c565ea5273ce2c08",
+    "url": "/matsiz/static/media/us2.bd68b88b.jpg"
+  },
+  {
+    "revision": "5cc7758da171352ca209c9db190f4d9b",
+    "url": "/matsiz/static/media/us3.5cc7758d.jpg"
+  }
+]);
